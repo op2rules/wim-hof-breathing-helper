680342f754dbf91db9851c28a09a2f111db9dda0
ejurga <emiljurga@hotmail.com>
2021-03-10 02:27:56 -0500

    Pulls ask_for_journal_entry() into main. Makes testing easier

--- application/src/wim-hof.py
+++ application/src/wim-hof.py
@@ -6,7 +6,8 @@ wim_hof_class = WimHofBreather()
 wim_hof_class.lets_do_some_breathing()
 
 log_manager = LogManager('breathing_log.tsv')
-log_manager.save_entry(wim_hof_class.get_breathing_times())
+journal_entry = log_manager.ask_for_journal_entry()
+log_manager.save_entry(journal_entry, wim_hof_class.get_breathing_times())
 log_manager.output_breathing_log(5)
 
 print("\nWim would be proud :)")
